319384d28a9937e63b03c7330397793d09e8c560
Simon McLoughlin <simon.d.mcl@gmail.com>
2021-10-22 15:24:36 +0100

    point to my version of torus to test mocking

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 		.package(name: "Sodium", url: "https://github.com/jedisct1/swift-sodium", from: "0.9.1"),
 		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
 		.package(name: "WalletCore", url: "https://github.com/hewigovens/wallet-core-spm", .branch("master")),
-		.package(name: "TorusSwiftDirectSDK", url: "https://github.com/torusresearch/torus-direct-swift-sdk", from: "1.1.1"),
+		.package(name: "TorusSwiftDirectSDK", url: "https://github.com/simonmcl/torus-direct-swift-sdk", .branch("mocking")),
 		.package(url: "https://github.com/simonmcl/SVGKit", from: "3.0.1"),
 		.package(name: "SignalRClient", url: "https://github.com/moozzyk/SignalR-Client-Swift", from: "0.8.0")
 	],
